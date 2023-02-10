66cbd5cdc43668976da81d610aad4c660ae7db63
IndrayaniKulkarni <indrayani.kulkarni25@gmail.com>
2023-02-10 20:05:53 +0530

    Modification Iteration2

--- GuessTheNumber/src/com/guessNum/Main.java
+++ GuessTheNumber/src/com/guessNum/Main.java
@@ -1,6 +1,7 @@
 package com.guessNum;
 
 import java.util.Scanner;
+
 /*
  *  This is guess number game.
  *  Player need to input any interger
@@ -13,20 +14,38 @@ import java.util.Scanner;
  */
 public class Main {
 
+	static int targetNum() {
+		int min = 1, max = 100;
+		int range = max - min + 1;
+		int target = 0;
+		// creating random number - which would be target
+		// this number is supposed to be guessed by player
+		for (int i = 0; i < 10; i++) {
+			target = (int) (Math.random() * range + min);
+		}
+		return target;
+	}
+
+	static boolean isItCorrectGuess(int target,int guess, boolean flag) {
+		// check is number correctly guessed else give hint
+		if (target == guess) {
+			System.out.println("\nBingo...You guessed right number .....");
+			flag = true;
+		} else if (target < guess) {
+			System.out.println("\nYour guessed number is high...Try again..");
+		} else if (target > guess) {
+			System.out.println("\nYour guessed number is low...Try again..");
+		}
+		return flag;
+	}
+
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
 		boolean flag = false;
 		int trials = 0;
-		int min = 1, max = 100;
-		int range = max - min + 1;
-		char choice='i';
-		int target = 0;
-		// creating random number - which would be target 
-		// this number is supposed to be guessed by player
-		for (int i = 0; i < 10; i++) {
-			target = (int) (Math.random() * range + min);
-		}
+		char choice = 'i';
+		int target = targetNum();
 		//player driven 
 		do {
 			System.out.println("________________________________________________");
@@ -35,16 +54,7 @@ public class Main {
 			int guess = sc.nextInt();
 
 			//System.out.println(target);
-			
-			// check is number correctly guessed else give hint
-			if (target == guess) {
-				System.out.println("\nBingo...You guessed right number .....");
-				flag = true;
-			} else if (target < guess) {
-				System.out.println("\nYour guessed number is high...Try again..");
-			} else if (target > guess) {
-				System.out.println("\nYour guessed number is low...Try again..");
-			}
+			flag = isItCorrectGuess(target,guess,flag);
 
 			System.out.println("________________________________________________");
 			
@@ -56,6 +66,9 @@ public class Main {
 			}
 			
 			trials++;
+			if(trials>=5) {
+				System.out.println("You ran out of number of trials..");
+			}
 
 		} while ((choice == 'y' || choice == 'Y') && (trials <= 5) && flag == false);
 		System.out.println("-----------------------EXIT-------------------------");
