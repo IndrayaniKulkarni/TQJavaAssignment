0089920050349cd311512c7a35b4730ab15b220b
IndrayaniKulkarni <indrayani.kulkarni25@gmail.com>
2023-02-14 13:39:29 +0530

    (i * 2 - 1) revisit

--- Assignments/src/com/assignment05_Patterns/TrianglePattern03.java
+++ Assignments/src/com/assignment05_Patterns/TrianglePattern03.java
@@ -20,20 +20,26 @@ package com.assignment05_Patterns;
 public class TrianglePattern03 {
 
 	public static void main(String[] args) {
-		
-		for(int i=1; i<=5; i++)
-		{
-			for(int j=1; j<(i+2); j++)
-			{
-				if(i==1 || j==1)
-				{
-					System.out.print(1);
-				}
-				else
-				{
-					System.out.print((j)+""+(j+1));
-				}
-				
+
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 1; j <= (i * 2 - 1); j++) {
+
+				System.out.print((j));
+				// +""+(j+1)
+				// j++;
+
+			}
+			System.out.println();
+		}
+
+		// lower half
+		for (int i = (5 - 1); i >= 1; i--) {
+			for (int j = 1; j <= (i * 2 - 1); j++) {
+
+				System.out.print((j));
+				// +""+(j+1)
+				// j++;
+
 			}
 			System.out.println();
 		}
